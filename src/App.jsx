a1676067493a5aa58e7b0ae95bc7b0e1472d51b1
MoaazHail <hailmoaaz@gmail.com>
2024-11-26 01:58:32 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Portfolio from './Components/Portfolio/Portfolio'
 import Navbar from './Components/Navbar/Navbar'
 import Home from './Components/Home/Home'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, createHashRouter, RouterProvider } from 'react-router-dom';
 import Layout from './Components/Layout/Layout';
 import ContactUs from './Components/Contact/Contact';
 import About from './Components/About/About';
@@ -13,7 +13,7 @@ import NotFound from './Components/NotFound/NotFound';
 export default function App() {
 
 
-    let router = createBrowserRouter([        // To Make Routing , We Should Download react-router-dom Library In npm  
+    let router = createHashRouter([        // To Make Routing , We Should Download react-router-dom Library In npm  
       {path:"",element:<Layout/>, children:[
         {index:true,element:<Home/>},
         {path:"about",element:<About/>},
