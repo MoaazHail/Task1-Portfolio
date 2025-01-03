0ff4c6384d59d12b0ce795bb6df6c0b0e442c989
MoaazHail <hailmoaaz@gmail.com>
2025-01-03 12:47:37 +0200

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
 
   return (
     <>
-      <nav className="bg-gray-800 z-20 fixed w-screen p-5 border-gray-200">
+      <nav className="bg-gray-800 z-20 fixed w-screen p-5 border-t-gray-200 border-t-4">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <Link to="" className="p-3 text-white font-bold text-4xl">
             START FRAMEWORK
@@ -14,9 +14,10 @@ export default function Navbar() {
           <div className=" md:hidden font-bold text-3xl border-gray-400">
             <i className="fa-solid fa-bars text-white p-2" onClick={()=>{setToggle(!toggle)}}></i>
           </div>
+
           {toggle && (
-            <div className="w-full md:block md:w-auto bg-gray-950">
-              <ul className=" flex flex-col p-4 md:p-0 mt-4 border  rounded-lg font-bold text-xl md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  ">
+            <div className="w-full md:block md:w-auto">
+              <ul className=" flex flex-col p-4 gap-5 md:p-0 mt-4 rounded-lg font-bold text-xl md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  ">
                 <li className=" list-none">
                   <NavLink to="about" className="p-3 text-white">
                     About
